cf8bac02037fee82d07e0af3bfaa87ea548dd5ef
Adam Simonini <adam.simonini@gmail.com>
2022-02-17 00:09:06 -0500

    fixed up aliasing

--- app/App.tsx
+++ app/App.tsx
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Stack, Container, Text, Link, HStack, Center, Heading, Switch, useColorMode, NativeBaseProvider, extendTheme, VStack, Code } from "native-base";
-import { BaseTheme } from "@theme";
-// import NativeBaseIcon from "@components/NativeBaseIcon";
+import { BaseTheme } from "./src/theme/index";
 // import VictoryChart from "./components/VictoryChart";
-// import SplayData from "@components/SplayData";
 import Root from "@components/Root";
 import API from "@utils/API";
 import config from "./nativebase.config";
--- app/babel.config.js
+++ app/babel.config.js
@@ -1,6 +1,26 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+// aliasing in expo: https://zohaib.me/using-path-aliases-in-react-native-typescript-created-with-expo/
+
+module.exports = function (api) {
+	api.cache(true);
+
+	const presets = ["babel-preset-expo"];
+	const plugins = [
+		[
+			"module-resolver",
+			{
+				root: ["."],
+				extensions: [".ios.ts", ".android.ts", ".ts", ".ios.tsx", ".android.tsx", ".tsx", ".jsx", ".js", ".json"],
+				alias: {
+					tests: ["./tests/"],
+					"@components": "./src/components",
+					"@theme": "./src/theme",
+					"@utils": "./src/utils"
+				}
+			}
+		]
+	];
+	return {
+		presets,
+		plugins
+	};
 };
