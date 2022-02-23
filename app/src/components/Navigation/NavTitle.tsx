997b5368cceb05c86d62cb07abb39dad9c936ad2
Adam Simonini <adam.simonini@gmail.com>
2022-02-23 09:25:03 -0500

    added translation for title

--- app/src/components/Navigation/NavTitle.tsx
+++ app/src/components/Navigation/NavTitle.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import { Flex, Image, Box, Heading, VStack, Spacer, Center } from "native-base";
+import { useTranslation } from "react-i18next";
 // import LanguageToggle from "@components/Navigation/LanguageToggle";
 
 // Replace navigation title with custom component: https://reactnavigation.org/docs/headers/#replacing-the-title-with-a-custom-component
 
 function NavTitle() {
+	const { t } = useTranslation();
 	return (
 		<Box w="100%" flex="1">
 			<VStack space={2.5} w="100%" px="3">
@@ -18,7 +20,7 @@ function NavTitle() {
 					alignItems="center"
 				>
 					<Image alt="public health trends logo" style={{ width: 50, height: 50 }} source={require("@assets/chart-600x600.png")} mr="3" />
-					<Heading size="lg">COVIDTrends</Heading>
+					<Heading size="lg">{t("COVIDTrends")}</Heading>
 				</Flex>
 			</VStack>
 		</Box>
